5deb4c9e48036ebd19ab832726bfab952307b05a
josephdocile <yusufuysalim@gmail.com>
2022-11-07 21:18:14 +0300

    update1

---
+++ Temel Atma Töreni/C Start.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void joseph1(){
+	printf("Hello World \n");
+	
+		printf("ANAN\n");
+	
+	printf("BABN\n");
+	
+	
+
+}
+
+void degiskenler(){
+	
+	int sayi=5;
+	double ondalik= 1.2;
+	float ondaliksi= 1.2;
+	char karakter='h';
+
+
+    //%d->decimal , %f->float, %s->string, %c->char
+	
+	
+	printf("%d \n",sayi );
+    printf("%c \n",karakter);
+	printf("%f \n",ondalik);
+    printf("%f \n",ondaliksi);
+	
+}
+
+
+
+
+
+
+int main(){
+	//joseph1();
+	degiskenler();
+	printf("josephten cikti \n");
+    system("pause");
+    
+}
+
+
